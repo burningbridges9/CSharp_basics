50e43a3d3cc08d37f84a33923a8379cb3138440a
burningbridges9 <talipov9@gmail.com>
2019-09-28 23:13:01 +0300

    Eleven task

---
+++ ElevenBuilding/Building.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenBuilding
+{
+    public class Building
+    {
+        static int count = 0;
+        private int number;
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = ++count;
+            }
+        }
+        public double Height { get; set; }
+        public int Floors { get; set; }
+        public int FlatsNumber { get; set; }
+        public int PorchNumber { get; set; }
+        internal Building(double Height, int Floors, int FlatsNumber, int PorchNumber)
+        {
+            Number = ++count;
+            this.Height = Height;
+            this.Floors = Floors;
+            this.FlatsNumber = FlatsNumber;
+            this.PorchNumber = PorchNumber;
+        }
+        internal Building()
+        {
+            Number = ++count;
+        }
+        internal Building(int Floors, int FlatsNumber, int PorchNumber)
+        {
+            Number = ++count;
+            this.Floors = Floors;
+            this.FlatsNumber = FlatsNumber;
+            this.PorchNumber = PorchNumber;
+        }
+        internal Building(int FlatsNumber, int PorchNumber)
+        {
+            Number = ++count;
+            this.Floors = Floors;
+            this.FlatsNumber = FlatsNumber;
+        }
+        public double GetFloorHeight()
+        {
+            return Height / Floors;
+        }
+        public int GetFlatsNumberInOnePorch()
+        {
+            return FlatsNumber / PorchNumber;
+        }
+        public int GetFlatsNumberInOneFloor()
+        {
+            return GetFlatsNumberInOnePorch() / Floors;
+        }
+        public override string ToString()
+        {
+            return "Building info:\n"
+                + Number.ToString() + '\n'
+                + Height.ToString() + '\n'
+                + Floors.ToString() + '\n'
+                + FlatsNumber.ToString() + '\n'
+                + PorchNumber.ToString() + '\n';
+        }
+    }
+}
---
+++ ElevenBuilding/FactoryBuilding.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenBuilding
+{
+    public class FactoryBuilding
+    {
+        static Hashtable hashtable = new Hashtable();
+        public static Building CreateBuilding()
+        {
+            Building building = new Building();
+            hashtable.Add(building.Number, building);
+            return building;
+        }
+        public static Building CreateBuilding(double Height, int Floors, int FlatsNumber, int PorchNumber)
+        {
+            Building building = new Building(Height, Floors, FlatsNumber, PorchNumber);
+            hashtable.Add(building.Number, building);
+            return building;
+        }
+        public static Building CreateBuilding(int Floors, int FlatsNumber, int PorchNumber)
+        {
+            Building building = new Building(Floors, FlatsNumber, PorchNumber);
+            hashtable.Add(building.Number, building);
+            return building;
+        }
+        public static Building CreateBuilding(int FlatsNumber, int PorchNumber)
+        {
+            Building building = new Building(FlatsNumber, PorchNumber);
+            hashtable.Add(building.Number, building);
+            return building;
+        }
+        public void DeleteBuilding(int number)
+        {
+            hashtable.Remove(number);
+        }
+        private FactoryBuilding() { }
+    }
+}
